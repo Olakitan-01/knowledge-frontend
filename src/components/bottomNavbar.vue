ef8048a99530c7b5abe37567c63e626520503b72
Olanrewaju Emmanuel <olanrewajuemma2023@gmail.com>
2026-03-16 17:43:09 +0100

    refactor: Navbar

--- src/components/bottomNavbar.vue
+++ src/components/bottomNavbar.vue
@@ -1,27 +1,26 @@
-<script>
-</script>
+<script></script>
 
 <template>
-    <nav class="bg-gray-800 border-t fixed bottom-0 w-full flex justify-around items-center py-3 px-2" 
-        style="padding-bottom: env(safe-area-inset-bottom)">
-        
-        <div class="flex items-center gap-20">
-            <router-link to="/">
-                <span class="material-icons">home</span>
-            </router-link>
-            <router-link to="/">
-                <span class="material-icons">movie</span>
-            </router-link>
-            <router-link to="/">
-                <span class="material-icons">send</span>
-            </router-link>
-            <router-link to="/">
-                <span class="material-icons">search</span>
-            </router-link>
-            <router-link to="/profile">
-                <span class="material-icons">account_circle</span>
-            </router-link>
-        
-        </div>
-    </nav>
+  <nav
+    class="bg-gray-800 border-t fixed bottom-0 w-full flex justify-around items-center py-3 px-2"
+    style="padding-bottom: env(safe-area-inset-bottom)"
+  >
+    <div class="flex items-center">
+      <router-link to="/">
+        <span class="material-icons">home</span>
+      </router-link>
+      <router-link to="/">
+        <span class="material-icons">movie</span>
+      </router-link>
+      <router-link to="/">
+        <span class="material-icons">send</span>
+      </router-link>
+      <router-link to="/">
+        <span class="material-icons">search</span>
+      </router-link>
+      <router-link to="/profile">
+        <span class="material-icons">account_circle</span>
+      </router-link>
+    </div>
+  </nav>
 </template>
